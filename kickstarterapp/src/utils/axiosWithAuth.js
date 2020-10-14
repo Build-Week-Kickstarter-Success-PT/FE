ee425ee3474e0cd44580e70e6f783990d4b19460
Brian Clayton <finalb@projectfinalboss.com>
2020-10-14 06:59:19 -0400

    add / update folders; add axios code

---
+++ kickstarterapp/src/utils/axiosWithAuth.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { getToken } from "./index.js";
+
+
+export const axiosWithAuth = () => {
+    const token = getToken();
+    return axios.create({
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+        baseURL: "",
+    });
+};
