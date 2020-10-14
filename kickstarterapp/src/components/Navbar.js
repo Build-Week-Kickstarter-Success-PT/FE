4df3c95fe652d4f8cb1e934765ab0f24925f9969
Ismail AlKamal <ismail@nawatt.com>
2020-10-14 14:18:25 +0300

    initial setup of components

---
+++ kickstarterapp/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import  { NavLink } from "react-router-dom";
+import logo from "../logo.png";
+import SearchIcon from '@material-ui/icons/Search';
+import "./Navbar.css"
+
+const Navbar = () => {
+    return ( 
+    <div className="navbar">
+        <div className="links__left links">
+            <NavLink to="">Explore</NavLink>
+            <NavLink to="">Start a Prediction</NavLink>
+        </div> 
+        <div className="links__center">
+            <img src={logo} alt="Kickstarter Success Predictor" />
+            <h2>Success Predictor</h2>
+        </div> 
+        <div className="links__right links">
+            <NavLink to="" style={{display:"flex", marginRight:"5px"}}><div>Search</div><SearchIcon style={{ fontSize: 17 , marginLeft: "5px", marginTop:"3px" }} /></NavLink>
+            <NavLink to="">Log in</NavLink>
+        </div> 
+    </div>
+    )
+}
+
+export default Navbar
